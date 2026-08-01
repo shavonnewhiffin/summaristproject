1b587333918bd270d534ddf42c9171cd5596fcca
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-07-31 20:24:41 -0700

    fix: moving header and footer to layout.js

--- app/page.js
+++ app/page.js
@@ -1,21 +1,18 @@
 import React from 'react'
-import Navbar from './components/Navbar'
 import Landing from './components/Landing'
 import Features from './components/Features'
 import Statistics from './components/Statistics';
 import Reviews from './components/Reviews'
 import Numbers from './components/Numbers';
-import Footer from './components/Footer';
 
 export default function Home() {
   return (
-    <><Navbar />
+    <>
       <Landing />
       <Features />
       <Statistics />
       <Reviews />
       <Numbers />
-      <Footer />
     </>
   );
 }
