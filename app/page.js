a8631511fb9ca488d3305b8c7b9cf8de2938aa8a
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-01 16:04:43 -0700

    feat: imported Home.js into page.js route

--- app/page.js
+++ app/page.js
@@ -1,18 +1,5 @@
-import React from 'react'
-import Landing from './components/Landing'
-import Features from './components/Features'
-import Statistics from './components/Statistics';
-import Reviews from './components/Reviews'
-import Numbers from './components/Numbers';
+import Home from './components/home/Home'
 
-export default function Home() {
-  return (
-    <>
-      <Landing />
-      <Features />
-      <Statistics />
-      <Reviews />
-      <Numbers />
-    </>
-  );
+export default function Page() {
+  return <Home />;
 }
