248e063bf5742869a95274038a6d955bc72eee9c
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-01 16:02:31 -0700

    fix: fixed some warped text from copy

--- app/components/Reviews.js
+++ app/components/Reviews.js
@@ -30,7 +30,7 @@ import { BsFillStarFill } from "react-icons/bs";
               </div>
               <div className={styles.review__body}>
                 I love this app! It provides
-                <b>concise and accurate summaries</b> of books in a way that is
+                <b> concise and accurate summaries</b> of books in a way that is
                 easy to understand. It's also very user-friendly and intuitive.
               </div>
             </div>
@@ -44,7 +44,7 @@ import { BsFillStarFill } from "react-icons/bs";
               <div className={styles.review__body}>
                 This app is a great way to get the main takeaways from a book
                 without having to read the entire thing.
-                <b>The summaries are well-written and informative.</b>
+                <b> The summaries are well-written and informative.</b>
                 Definitely worth downloading.
               </div>
             </div>
@@ -57,7 +57,7 @@ import { BsFillStarFill } from "react-icons/bs";
               </div>
               <div className={styles.review__body}>
                 If you're a busy person who
-                <b>loves reading but doesn't have the time</b> to read every
+                <b> loves reading but doesn't have the time</b> to read every
                 book in full, this app is for you! The summaries are thorough
                 and provide a great overview of the book's content.
               </div>
