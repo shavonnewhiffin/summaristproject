e7972687eb5be34138e4159131240dd9a92ae513
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-07-31 19:11:03 -0700

    feat: added statistics component to home

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Landing from './components/Landing'
 import Features from './components/Features'
+import Statistics from './components/Statistics';
 
 export default function Home() {
   return (
     <><Navbar />
       <Landing />
       <Features />
+      <Statistics />
     </>
   );
 }
