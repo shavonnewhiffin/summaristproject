88cd704ccd49a5d5f96914d8509d9b8f9d9443ee
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-07-31 18:41:12 -0700

    feat: implemented features section

---
+++ app/components/Features.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styles from '../styles/Features.module.css'
+import { AiFillFileText, AiFillBulb, AiFillAudio } from 'react-icons/ai'
+
+export default function Features() {
+  return (
+    <section id="features">
+              <div className="container">
+        <div className="row">
+          <div className="section__title">Understand books in few minutes</div>
+          <div className={styles.features__wrapper}>
+            <div className={styles.features}>
+              <div className={styles.features__icon}>
+                <AiFillFileText />
+              </div>
+              <div className={styles.features__title}>Read or listen</div>
+              <div className={styles['features__sub--title']}>
+                Save time by getting the core ideas from the best books.
+              </div>
+            </div>
+            <div className={styles.features}>
+              <div className={styles.features__icon}>
+                <AiFillBulb />
+              </div>
+              <div className={styles.features__title}>Find your next read</div>
+              <div className={styles['features__sub--title']}>
+                Explore book lists and personalized recommendations.
+              </div>
+            </div>
+            <div className={styles.features}>
+              <div className={styles.features__icon}>
+                <AiFillAudio />
+              </div>
+              <div className={styles.features__title}>Briefcasts</div>
+              <div className={styles['features__sub--title']}>
+                Gain valuable insights from briefcasts
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
