28b22b6eae3b748498393bdd97f726297a918411
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-01 16:04:16 -0700

    feat: compiled all home components into Home.js to be imported into page.js

---
+++ app/components/home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Landing from '../Landing'
+import Features from '../Features'
+import Statistics from '../Statistics'
+import Reviews from '../Reviews'
+import Numbers from '../Numbers'
+
+export default function Home() {
+  return (
+    <>
+      <Landing />
+      <Features />
+      <Statistics />
+      <Reviews />
+      <Numbers />
+    </>
+  );
+}
