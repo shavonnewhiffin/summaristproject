150f3b3e9b780e4130149858046103645da1f3e0
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-05 17:17:02 -0700

    fix: moved navbar and footer to home page only

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -4,15 +4,19 @@ import Features from '../Features'
 import Statistics from '../Statistics'
 import Reviews from '../Reviews'
 import Numbers from '../Numbers'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
 
 export default function Home() {
   return (
     <>
+      <Navbar/>
       <Landing />
       <Features />
       <Statistics />
       <Reviews />
       <Numbers />
+      <Footer />
     </>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from '../app/components/Footer'
-import NavBar from '../app/components/Navbar'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +23,8 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <NavBar />
         {children}
-        <Footer /></body>
+      </body>
     </html>
   );
 }
