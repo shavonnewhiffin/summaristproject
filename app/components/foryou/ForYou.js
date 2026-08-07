804913aeed424cb51ad077399030e3099a3dd698
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-06 20:30:33 -0700

    feat: imported Selected and Recommended components

--- app/components/foryou/ForYou.js
+++ app/components/foryou/ForYou.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Sidebar from './Sidebar'
 import Search from './Search'
 import Selected from './Selected'
+import Recommended from './Recommended'
 import styles from '../../styles/for-you/ForYou.module.css'
 
 const ForYou = () => {
@@ -13,6 +14,7 @@ const ForYou = () => {
         <div className="row">
           <div className="container">
             <Selected />
+            <Recommended />
           </div>
         </div>
       </div>
