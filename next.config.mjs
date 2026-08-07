c7e21a380d3a7beee4b60e6842b6d0189a51f294
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-06 18:24:37 -0700

    fix: enabled firebase images

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
