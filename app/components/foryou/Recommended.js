cb594644e9df143e8dd198ead2233522acadd355
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-08 16:12:37 -0700

    fix: changed for-you to for you so make books line up in display flex on wrapper

--- app/components/foryou/Recommended.js
+++ app/components/foryou/Recommended.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import styles from '../../styles/for-you/ForYou.module.css'
+import BookCard from './BookCard'
 
-const Recommended = () => {
+export default async function Recommended() {
+const response = await fetch('https://us-central1-summaristt.cloudfunctions.net/getBooks?status=recommended');
+const data = await response.json();
+const books = data.slice(0, 5);
+
   return (
     <div>
         <div className="title">Recommended for you</div>
         <div className="sub-title">We think you'll like these</div>
         <div className={styles['foryou__recommended--books']} >
-            Book
+           {books.map((book) => (<BookCard key={book.id} book={book} />))}
         </div>
     </div>
   )
 }
 
-export default Recommended
