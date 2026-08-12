fd1ab110059b802322662671c48df668a6e2481f
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-12 16:44:26 -0700

    feat: added a route to player page

--- app/(dashboard)/book/[id]/page.js
+++ app/(dashboard)/book/[id]/page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link'
 import { IoIosStarOutline } from "react-icons/io";
 import { GoClock } from "react-icons/go";
 import { IoMicOutline } from "react-icons/io5";
@@ -54,18 +55,18 @@ export default async function page({ params }) {
           </div>
            </div>
           <div className={styles['inner__book--read-btn--wrapper']}>
-            <button className={styles['inner__book--read-btn']}>
+            <Link href={`/book/${book.id}/player`} className={styles['inner__book--read-btn']}>
               <div className={styles.readbtn__icon}>
                 <IoBookOutline />
               </div>
               <div className={styles.readbtn__text}>Read</div>
-            </button>
-            <button className={styles['inner__book--read-btn']}>
+            </Link>
+            <Link href={`/book/${book.id}/player`}  className={styles['inner__book--read-btn']}>
               <div className={styles.readbtn__icon}>
               <IoMicOutline />
               </div>
               <div className={styles.readbtn__text}>Listen</div>
-            </button>
+            </Link>
           </div>
           <div className={styles['inner__book--bookmark']}>
             <div className={styles['inner__book--bookmark-icon']}>
