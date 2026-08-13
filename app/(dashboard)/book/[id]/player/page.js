e8702250a5a442d3d2a762c5388da8884aa203ff
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-12 17:45:26 -0700

    feat: implemented player page design and rendered fetched data

--- app/(dashboard)/book/[id]/player/page.js
+++ app/(dashboard)/book/[id]/player/page.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import styles from "../../../../styles/dashboard/Player.module.css"
+import { TbRewindBackward10 } from "react-icons/tb";
+import { TbRewindForward10 } from "react-icons/tb";
+import { FaPlayCircle } from "react-icons/fa";
 
-const page = () => {
+
+export default async function player ({params}) {
+ const { id } = await params;
+  const response = await fetch(
+    `https://us-central1-summaristt.cloudfunctions.net/getBook?id=${id}`,
+  );
+  const book = await response.json();
+  console.log(book);
+
   return (
-    <div>page</div>
-  )
-}
+    <div className={styles.summary}>
+      <div className={styles.audiobook__summary}>
+        <div className={styles['audiobook__summary--title']}>
+          <b>{book.title}</b>
+        </div>
+        <div className={styles['audiobook__summary--text']}>{book.summary}</div>
+      </div>
+       <div className={styles.audio__wrapper}>
+        <audio src=""></audio>
+        <div className={styles['audio__track--wrapper']}>
+          <figure className={styles['audio__track--image-mask']}>
+            <figure className={styles['book__image--wrapper']}>
+              <img className={styles.book__image}></img>
+            </figure>
+          </figure>
+          <div className={styles['audio__details--wrapper']}>
+            <div className={styles['audio__track--title']}>{book.title}</div>
+            <div className={styles['audio__track--author']}>{book.author}</div>
+          </div>
+        </div>
+        <div className={styles['audio__controls--wrapper']}>
+          <div className={styles.audio__controls}>
+            <button className={styles['audio__controls--btn']}>
+            <TbRewindBackward10 />
+            </button>
+            <button className={`${styles['audio__controls--btn']} ${styles['audio__controls--btn-play']}`}>
+             <FaPlayCircle />
+            </button>
+            <button className={styles['audio__controls--btn']}>
+            <TbRewindForward10 />
+            </button>
+          </div>
+        </div>
+        <div className={styles['audio__progress--wrapper']}>
+            <div className={styles.audio__time}>
+                00:00
+            </div>
+            <input type="range" className={styles['audio__progress--bar']} />
+            <div className={styles.audio__time}>03:24</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
