060ca235157b36e338da169367fe197712522be5
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-13 17:56:20 -0700

    feat: updated page

---
+++ app/ReduxProvider.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
+
+
+
+export default function ReduxProvider({ children }) {
+  return (
+   <Provider store={store}>
+    {children}
+    </Provider>
+  )
+}
