4ac76729e4551ca38c3007429c562b8c6cb1a6e8
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-13 17:52:07 -0700

    feat: implemented audio player

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ReduxProvider from "./ReduxProvider";
+import { AudioPlayerProvider } from "./src/context/audio-player-context";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,11 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+         <ReduxProvider>
+           <AudioPlayerProvider>
         {children}
+          </AudioPlayerProvider>
+        </ReduxProvider>
       </body>
     </html>
   );
