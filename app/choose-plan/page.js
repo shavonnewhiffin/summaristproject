3f12267e9b17da582e836cd058dd3733696b34e1
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-14 18:46:04 -0700

    feat: finished adding components to choose-plan

--- app/choose-plan/page.js
+++ app/choose-plan/page.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Hero from "../components/chooseplan/Hero";
 import Plans from "../components/chooseplan/Plans";
+import Accordion from "../components/chooseplan/Accordion";
+import Footer from "../components/Footer";
 
 const page = () => {
   return (
     <>
       <Hero />
       <Plans />
+      <Accordion />
+      <Footer />
+      
     </>
   );
 };
