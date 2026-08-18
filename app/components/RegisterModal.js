34bda9ae7e4f8e73c68ca07f7104b44e6a87f4bc
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-18 15:27:41 -0700

    feat: implementing firebase, modals, spinner loading state

---
+++ app/components/RegisterModal.js
@@ -0,0 +1,85 @@
+"use client";
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { auth } from '../src/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React from 'react'
+import { IoMdClose } from "react-icons/io";
+import { FaSpinner } from "react-icons/fa";
+import styles from '../styles/home/Modals.module.css'
+import Image from 'next/image'
+import google from '../.././public/images/google.png'
+
+const RegisterModal = ({ onClose, onLogin }) => {
+    const [user, setUser] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setAuthError] = useState(null);
+
+    const router = useRouter();
+
+       function register() {
+        setLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((user) => {
+            console.log(user);
+            setUser(user)
+            onClose();
+            router.push('/for-you');
+         
+        })
+        .catch((error) => {
+            console.log(error);
+            setLoading(false);
+            setAuthError(error.message);
+        })
+
+    }
+       function handleSubmit(e) {
+        e.preventDefault();
+        register();
+    }
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <div className={styles['modal__content']}>
+          <div className={styles['modal__title']}>Sign Up with Summarist</div>
+          {error && <div className={styles['auth__error']}>{error}</div>}
+          <button className={`btn ${styles.btn} ${styles['google__btn--wrapper']}`} onClick={() => register()}>
+            <figure className={styles['google__icon--mask']}>
+              <Image src={google} alt="" />
+            </figure>
+            <div className="">Register with Google</div>
+          </button>
+          <div className={styles['modal__seperator']}>
+            <span className={styles['modal__seperator--text']}>or</span>
+          </div>
+          <form className={styles['modal__main--form']} onSubmit={handleSubmit}>
+            <input
+              className={styles['modal__main--input']}
+              type="text"
+              placeholder="Email Address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className={styles['modal__main--input']}
+              type="password"
+              placeholder="Password"
+              onChange = {(e) => setPassword(e.target.value) }
+            />
+            <button className="btn">
+              {loading? (<FaSpinner  className="spinner"/>) : (<span>Register</span>)}
+            </button>
+          </form>
+        </div>
+        <div className={styles['modal__switch--btn']} onClick={onLogin}>Already have an account?</div>
+        <div className={styles['modal__close--btn']}>
+          <IoMdClose onClick={onClose} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RegisterModal
---
+++ app/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANbtgE8e90SOeeuDrOYmgbjIKd6y8nD1E",
+  authDomain: "summarist-1b0ca.firebaseapp.com",
+  projectId: "summarist-1b0ca",
+  storageBucket: "summarist-1b0ca.firebasestorage.app",
+  messagingSenderId: "1069704224693",
+  appId: "1:1069704224693:web:68e06160b6e523c08ec3fc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
