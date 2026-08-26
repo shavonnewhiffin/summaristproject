c040992a6470510180ac2ae3ba7843cd1f307200
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-08-25 22:04:42 -0700

    feat: implemented code for my library page

--- app/(dashboard)/my-library/page.js
+++ app/(dashboard)/my-library/page.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styles from '../../styles/for-you/ForYou.module.css'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="container">
+        <div className="row">
+            <div className="title">
+                Saved Books
+            </div>
+            <div className={styles['foryou__subtitle']}>
+                2 items
+            </div>
+            <div className={styles['foryou__books--wrapper']}>
+                
+            </div>
+            <div className="title">
+                Finished
+            </div>
+            <div className={styles['foryou__subtitle']}>
+                13 items
+            </div>
+            <div className={styles['foryou__books--wrapper']}>
+
+            </div>
+        </div>
+        </div>
   )
 }
 
