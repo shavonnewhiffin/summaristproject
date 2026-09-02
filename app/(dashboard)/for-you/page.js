9d692924863e29529bf0fbac1637f7ae1e9a5ca1
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-09-01 22:21:29 -0700

    feat: if user not subscribed show choose-plan

--- app/(dashboard)/for-you/page.js
+++ app/(dashboard)/for-you/page.js
@@ -1,5 +1,14 @@
 import ForYou from '../../components/foryou/ForYou'
+import Selected from '../../components/foryou/Selected'
+import Recommended from '../../components/foryou/Recommended'
+import Suggested from '../../components/foryou/Suggested'
 
 export default function Page() {
-  return <ForYou />;
+  return (
+    <ForYou>
+      <Selected />
+      <Recommended />
+      <Suggested />
+    </ForYou>
+  );
 }
