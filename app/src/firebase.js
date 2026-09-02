f7e6677fc5233b3048641a74c0d325cf3ae2567e
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-09-01 17:33:23 -0700

    feat: implemented checkout feature

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyANbtgE8e90SOeeuDrOYmgbjIKd6y8nD1E",
   authDomain: "summarist-1b0ca.firebaseapp.com",
@@ -15,8 +11,8 @@ const firebaseConfig = {
   appId: "1:1069704224693:web:68e06160b6e523c08ec3fc"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
