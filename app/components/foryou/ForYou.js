1c5da73fcbf9cd15254dcd6ebe9c25d63e1a0859
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-09-03 15:01:46 -0700

    fix: removed redirect to choose-plan

--- app/components/foryou/ForYou.js
+++ app/components/foryou/ForYou.js
@@ -1,20 +1,6 @@
 "use client";
 
-import React, { useContext, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import { AuthContext } from '../../src/context/auth-context'
-
 const ForYou = ({ children }) => {
-  const { isPremium, loading } = useContext(AuthContext)
-  const router = useRouter()
-
-  useEffect(() => {
-    if (!loading && !isPremium) {
-      router.push('/choose-plan')
-    }
-  }, [loading, isPremium, router])
-
-  if (loading || !isPremium) return null
 
   return (
     <div>
