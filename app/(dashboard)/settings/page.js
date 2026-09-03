15126311d4027c9a2a17b1d28d847278b7f44e40
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-09-03 15:02:32 -0700

    feat: implemented upgrade to premium button

--- app/(dashboard)/settings/page.js
+++ app/(dashboard)/settings/page.js
@@ -1,12 +1,27 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import Link from 'next/link'
 import { AuthContext } from "../../src/context/auth-context";
 import styles from "../../styles/dashboard/Settings.module.css";
 import LoginPrompt from '../../components/dashboard/LoginPrompt';
+import { getPremiumStatus } from '../../components/chooseplan/GetPremiumStatus'
 
 const page = () => {
   const { user, loading } = useContext(AuthContext);
+  const [isPremium, setIsPremium] = useState(false);
+
+useEffect(() => {
+  if (!user) return;
+
+  const checkPremium = async () => {
+    const status = await getPremiumStatus();
+    setIsPremium(status);
+  }
+
+  checkPremium();
+},[user])
+
   console.log(user);
 
   if (loading) return null;
@@ -21,7 +36,10 @@ const page = () => {
           <div className={styles["settings__sub-title"]}>
             Your Subscription Plan
           </div>
-          <div className={styles["settings__text"]}>Premium</div>
+          <div className={styles["settings__text"]}>{isPremium? "Premium" : "Basic"}</div>
+          {!isPremium? (<Link className={`${styles["settings__upgrade--btn"]} ${styles['btn']}`} href="/choose-plan">
+            Upgrade to Premium
+            </Link> ) : null}
         </div>
         <div className={styles["settings__content"]}>
           <div className={styles["settings__sub-title"]}>Email</div>
