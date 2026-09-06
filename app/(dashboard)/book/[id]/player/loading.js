cef6b60fc143cdb567a4b4d86ce10b5804aba1ce
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-09-05 20:54:51 -0700

    feat: added audio player loading page

---
+++ app/(dashboard)/book/[id]/player/loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AudioLoading } from '../../../../components/skeletons/AudioLoading'
+
+export default function loading() {
+  return (
+    <>
+      <AudioLoading />
+    </>
+  )
+}
