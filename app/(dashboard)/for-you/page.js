1c1ca6afcee82687ae1e4ea00bb83b511f3c01ca
shavonnewhiffin <shavonnewhiffin@gmail.com>
2026-09-05 20:31:08 -0700

    feat: implemented loading state

--- app/(dashboard)/for-you/page.js
+++ app/(dashboard)/for-you/page.js
@@ -1,14 +1,25 @@
+
+import { Suspense } from 'react'
 import ForYou from '../../components/foryou/ForYou'
 import Selected from '../../components/foryou/Selected'
 import Recommended from '../../components/foryou/Recommended'
 import Suggested from '../../components/foryou/Suggested'
+import { SelectedSkeleton } from '../../components/skeletons/SelectedSkeleton'
+import { RecommendedSkeleton } from '../../components/skeletons/RecommendedSkeleton'
+import { SuggestedSkeleton } from '../../components/skeletons/SuggestedSkeleton'
 
 export default function Page() {
   return (
     <ForYou>
-      <Selected />
-      <Recommended />
-      <Suggested />
+      <Suspense fallback={<SelectedSkeleton />}>
+        <Selected />
+      </Suspense>
+      <Suspense fallback={<RecommendedSkeleton />}>
+        <Recommended />
+      </Suspense>
+      <Suspense fallback={<SuggestedSkeleton />}>
+        <Suggested />
+      </Suspense>
     </ForYou>
   );
 }
